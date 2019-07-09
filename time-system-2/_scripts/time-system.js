2dde011a09200886acf784312653e84e7c43c18a
Caio César <br.caiorecife@cesarleitao.com.br>
2019-07-08 23:16:39 -0300

    time-system-2 files

--- time-system-2/_scripts/time-system.js
+++ time-system-2/_scripts/time-system.js
@@ -3,11 +3,14 @@ var time = document.getElementById("time")
 var msg = document.getElementById("msg")
 var data = new Date()
 
-var body = document.getElementById("body")
-var header2 = document.getElementById("header")
+var body = document.getElementById("corpo")
+var header = document.getElementById("menu")
+var click = document.getElementsByClassName("menuclick")
+var img = document.getElementById("photo")
+
 
 // var hora = data.getHours()
-var hora = 2
+var hora = 24
 var minuto = data.getMinutes()
 var dia = data.getDate()
 
@@ -16,19 +19,25 @@ time.innerHTML = `Day ${dia}<br/>${hora} hours`
 if(hora >= 0 && hora < 12){
 
 msg.innerHTML = "Bom dia!"
-body.style.backgroundColor = "#ddd"
-time.style.color = "#000"
-header2.style.backgroundColor = "fff"
-
+body.style.backgroundColor = "#dddddd"
+header.style.backgroundColor = "#ffffff"
+time.style.color = "#fff"
 
 } else if(hora >= 12 && hora < 18){
 
-msg.innerHTML = "Boa tarde!"
+    msg.innerHTML = "Bom dia!"
+    body.style.backgroundColor = "#dddddd"
+    time.style.color = "#fff"
+    msg.innerHTML = "Boa tarde!"
 
 }else{
 
 msg.innerHTML = "Boa noite!"
-
+msg.style.color = "#fff"
+body.style.backgroundColor = "#121212"
+header.style.backgroundColor = "#272727"
+img.src = "_data/night.jpg"
+img.style.marginLeft = "-180px"
 }
 
 }
